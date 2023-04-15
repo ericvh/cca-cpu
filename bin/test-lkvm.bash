eed35bc2c864e1607d9fcba97a31864c6363676c
Eric Van Hensbergen <ericvh@kernel.org>
2023-04-15 00:56:37 +0000

    Bump memory size and reduce u-root size
    We were running into memory problems which were causing the realm to
    not boot.  (unclear why this wasn't happening before).  This patch grows
    memory to 256M and just includes elvish and cpud in the initrd cpio.
    
    Signed-off-by: Eric Van Hensbergen <ericvh@kernel.org>

--- bin/test-lkvm.bash
+++ bin/test-lkvm.bash
@@ -2,5 +2,5 @@
 FVP_IP=`docker inspect fvp | jq -r '.[].NetworkSettings.IPAddress'`
 ln -f -s /tmp/local/tmp $HOME/.lkvm
 PWD=/ /workspaces/artifacts/cpu -namespace "/workspaces:/lib:/lib64:/usr:/bin:/etc:/home" $FVP_IP /workspaces/artifacts/lkvm run --realm --irqchip=gicv3-its --console=virtio \
-    -c 1 -m 64 \
+    -c 1 -m 256 \
     -k /workspaces/artifacts/Image.guest -i /workspaces/artifacts/initramfs.cpio
