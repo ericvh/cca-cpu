7dc687e11fca6311d95329fdcdb56bac1d57417b
Eric Van Hensbergen <ericvh@kernel.org>
2023-04-25 15:11:22 +0000

    Enable networking in realm
    Signed-off-by: Eric Van Hensbergen <ericvh@kernel.org>

--- bin/test-lkvm.bash
+++ bin/test-lkvm.bash
@@ -4,5 +4,4 @@
 #PWD=/ /workspaces/artifacts/cpu -namespace "/workspaces:/lib:/lib64:/usr:/bin:/etc:/home" $FVP_IP /workspaces/artifacts/lkvm run --realm --irqchip=gicv3-its --console=virtio \
 #    -c 1 -m 256 \
 #    -k /workspaces/artifacts/Image.guest -i /workspaces/artifacts/initramfs.cpio
-
-/mnt/workspaces/artifacts/lkvm run --realm --irqchip=gicv3-its --console=virtio -c 1 -m 256 -k /mnt/workspaces/artifacts/Image.guest -i /mnt/workspaces/artifacts/initramfs.cpio --9p /mnt,FM --rng
+/mnt/workspaces/artifacts/lkvm run --realm --irqchip=gicv3-its --console=virtio -c 1 -m 256 -k /mnt/workspaces/artifacts/Image.guest -i /mnt/workspaces/artifacts/initramfs.cpio --9p /mnt,FM --rng -p "ip=on"
